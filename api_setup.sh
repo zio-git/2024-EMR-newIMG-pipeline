157653a1a698d56da54e18f3f7ee41dd41e36bcd
Your Name <you@example.com>
2024-10-14 13:48:13 +0000

    addint FBC command

--- api_setup.sh
+++ api_setup.sh
@@ -2,7 +2,7 @@
 set -x # Enable debugging
 echo "pulling and Checkout API tag-------------------------------------------"
 git fetch --tags jenkins@10.44.0.51:/var/lib/jenkins/workspace/2024-Q3-EMR-pipeline-newIMG-1/BHT-EMR-API -f
-git checkout v5.3.0-beta -f
+git checkout v5.3.0 -f
 git describe > HEAD
 echo "____________________________________________"
 export PATH=$PATH:/home/emr-user/.rbenv/bin
@@ -17,6 +17,7 @@ bundle install --local
 echo "--------------------------------------------"
 rm -rf /var/www/BHT-EMR-API/db/migrate/20190527130805_create_uuid_remaps.rb
 echo "running bin_update art"
+RAILS_ENV=production rails r bin/fbc_migration.rb
 echo "____________________________________________"
 ./bin/update_art_metadata.sh production
 echo "____________________________________________"
